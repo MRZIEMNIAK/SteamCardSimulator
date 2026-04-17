58114623786f26da11cd2790db115adf03415e71
turboveikka <veikka.weckman@edu.turkuamk.fi>
2026-04-17 21:49:11 +0300

    lsit of games and cards

---
+++ state.py
@@ -0,0 +1,61 @@
+#game state management
+
+#list of games
+games = [
+    {"id": 1, "name": "DOTA 2"},
+    {"id": 2, "name": "Team Fortress 2"},
+    {"id": 3, "name": "Counter-Strike: Global Offensive"},
+    {"id": 4, "name": "PAYDAY 2"},
+    {"id": 5, "name": "Portal 2"},
+    {"id": 6, "name": "Left 4 Dead 2"},
+        ]
+
+cards = {
+    1: [ #DOTA 2 cards: 
+        {id: 1, "name": "DOTA 2 Card"},
+        {id: 2, "name": "DOTA 2 Card"},
+        {id: 3, "name": "DOTA 2 Card"},
+        {id: 4, "name": "DOTA 2 Card"},
+        {id: 5, "name": "DOTA 2 Card"},
+    ],
+    2: [ #Team Fortress 2 cards:
+        {id: 1, "name": "Team Fortress 2 Card"},
+        {id: 2, "name": "Team Fortress 2 Card"},
+        {id: 3, "name": "Team Fortress 2 Card"},
+        {id: 4, "name": "Team Fortress 2 Card"},
+        {id: 5, "name": "Team Fortress 2 Card"},
+    ],
+    3: [ #Counter-Strike: Global Offensive cards:
+        {id: 1, "name": "Counter-Strike: Global Offensive Card"},
+        {id: 2, "name": "Counter-Strike: Global Offensive Card"},
+        {id: 3, "name": "Counter-Strike: Global Offensive Card"},
+        {id: 4, "name": "Counter-Strike: Global Offensive Card"},
+        {id: 5, "name": "Counter-Strike: Global Offensive Card"},
+    ],
+    4: [ #PAYDAY 2 cards:
+        {id: 1, "name": "PAYDAY 2 Card"},
+        {id: 2, "name": "PAYDAY 2 Card"},
+        {id: 3, "name": "PAYDAY 2 Card"},
+        {id: 4, "name": "PAYDAY 2 Card"},
+        {id: 5, "name": "PAYDAY 2 Card"},
+    ],
+    5: [ #Portal 2 cards:
+        {id: 1, "name": "Portal 2 Card"},
+        {id: 2, "name": "Portal 2 Card"},
+        {id: 3, "name": "Portal 2 Card"},
+        {id: 4, "name": "Portal 2 Card"},
+        {id: 5, "name": "Portal 2 Card"},
+    ],
+    6: [ #Left 4 Dead 2 cards:
+        {id: 1, "name": "Left 4 Dead 2 Card"},
+        {id: 2, "name": "Left 4 Dead 2 Card"},
+        {id: 3, "name": "Left 4 Dead 2 Card"},
+        {id: 4, "name": "Left 4 Dead 2 Card"},
+        {id: 5, "name": "Left 4 Dead 2 Card"}
+    ]
+
+}
+
+
+
+
